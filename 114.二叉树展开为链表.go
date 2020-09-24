e3debd787c9038c4ef45e42a2a40a8cf8378b038
fibbery <jiangnenghua1992@gmail.com>
2020-09-24 23:12:22 +0800

    114 new

--- 114.二叉树展开为链表.go
+++ 114.二叉树展开为链表.go
@@ -20,21 +20,38 @@
 // 	Left, Right *TreeNode
 // }
 
+// func flatten(root *TreeNode) {
+// 	var preorder func(node *TreeNode) []*TreeNode
+// 	preorder = func(node *TreeNode) (nodes []*TreeNode) {
+// 		if node == nil {
+// 			return
+// 		}
+// 		nodes = append(nodes, node)
+// 		nodes = append(nodes, preorder(node.Left)...)
+// 		nodes = append(nodes, preorder(node.Right)...)
+// 		return
+// 	}
+// 	data := preorder(root)
+// 	for i := 0; i < len(data)-1; i++ {
+// 		data[i].Left, data[i].Right = nil, data[i+1]
+// 	}
+// }
+
 func flatten(root *TreeNode) {
-	var preorder func(node *TreeNode) []*TreeNode
-	preorder = func(node *TreeNode) (nodes []*TreeNode) {
-		if node == nil {
-			return
-		}
-		nodes = append(nodes, node)
-		nodes = append(nodes, preorder(node.Left)...)
-		nodes = append(nodes, preorder(node.Right)...)
+	if root.Left == nil && root.Right == nil {
 		return
 	}
-	data := preorder(root)
-	for i := 0; i < len(data)-1; i++ {
-		data[i].Left, data[i].Right = nil, data[i+1]
+	left := root.Left
+	right := root.Right
+	flatten(left)
+	flatten(right)
+
+	root.Left, root.Right = nil, left
+	node := left
+	for node.Right != nil {
+		node = node.Right
 	}
+	node.Right = right
 }
 
 // @lc code=end
